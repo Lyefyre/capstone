796d46b79bac8712436f532b0f47a1d81f047f9a
Lyefyre <v.schaer@gmx.ch>
2019-01-12 08:29:56 +0000

    created linkedList class, implemented basic node functions

--- script.py
+++ script.py
@@ -13,6 +13,28 @@ class Node():
         self.next_node = next_node
 
 
+class LinkedList():
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def getHeadNode(self):
+        return self.head_node
+
+    def insertBeginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.setNextNode(self.next_node)
+        self.head_node = new_node
+
+    def stringifyList(self):
+        string_list = ""
+        merker = self.getHeadNode()
+        while merker:
+            if merker.getValue() is not None:
+                string_list += str(merker.getValue()) + "\n"
+            merker = merker.getNextNode()
+        return string_list
+
+
 print("Welcome to the SoHo Restauran!")
 print("""What type of food would you like to eat?
 Type the beginning of the food to see, if it is available!""")
