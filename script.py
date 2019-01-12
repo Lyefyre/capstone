a7d3e759e0b803844259b1b320e58500b7218ae2
Lyefyre <v.schaer@gmx.ch>
2019-01-12 08:03:21 +0000

    Created class Node und basic functions

--- script.py
+++ script.py
@@ -1,3 +1,18 @@
+class Node():
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def getValue(self):
+        return self.value
+
+    def getNextNode(self):
+        return self.next_node
+
+    def setNextNode(self, next_node):
+        self.next_node = next_node
+
+
 print("Welcome to the SoHo Restauran!")
 print("""What type of food would you like to eat?
 Type the beginning of the food to see, if it is available!""")
